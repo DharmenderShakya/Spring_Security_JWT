ea90dca91d3d3f1385b5a71691820f89bd47b23a
DHARMENDER SHAKYA <110955017+DharmenderShakya@users.noreply.github.com>
2026-04-08 11:39:03 +0530

    Add files via upload

---
+++ Spring_Security_JWT/src/test/java/com/Spring_Security_JWT/TestSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.Spring_Security_JWT;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@TestConfiguration
+@EnableMethodSecurity
+ class TestSecurityConfig {
+}
